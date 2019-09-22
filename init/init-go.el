dfb479fcd409f9444b885acb9597dbf48c208e7f
larte <lauri.arte@gmail.com>
2019-09-22 10:28:27 +0300

    go-vet fixes

--- init/init-go.el
+++ init/init-go.el
@@ -1,5 +1,10 @@
 (require-package 'go-mode)
 
 (add-hook 'before-save-hook #'gofmt-before-save)
+(add-to-list 'exec-path (expand-file-name "~/go/bin/godef") )
+
+(let ((govet (flycheck-checker-get 'go-vet 'command)))
+  (when (equal (cadr govet) "tool")
+    (setf (cdr govet) (cddr govet))))
 
 (provide 'init-go)
